1a8aa0ecafab54c4a71f010839df5b602caea0a4
CurtisJamesPhan <curtisjamesphan@gmail.com>
2019-02-22 16:18:20 -0800

    one

---
+++ app/proptypes.js
@@ -0,0 +1,46 @@
+// var React = require('react');
+// var ReactDom = require('react-dom');
+// var PropTypes = require('prop-types');
+
+// class Avatar extends React.Component {
+//     render() {
+//         return (
+//             <img src={this.props.img} />
+//         )
+//     }
+// }
+
+// class Label extends React.Component {
+//     render() {
+//         return (
+//             <h1>Name: {this.props.name}</h1>
+//         )
+//     }
+// }
+
+// class ScreenName extends React.Component {
+//     render() {
+//         return (
+//             <h3>Username: {this.props.username}</h3>
+//         )
+//     }
+// }
+
+// class Badge extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Avatar img={this.props.user.img} />
+//                 <Label name={this.props.user.name} />
+//                 <ScreenName username={this.props.user.username} />
+//             </div>
+//         )
+//     }
+// }
+
+
+
+// ReactDOM.render(
+//     <Badge user={{ name: 'Tyler McGs', img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460', username: 'tylermcginnis' }} />,
+//     document.getElementById('app')
+// );
